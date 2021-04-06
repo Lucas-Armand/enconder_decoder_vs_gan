1794637fa0a8dcf5acca81e092a35e0dbc15975e
Lucas-Armand <oliveira.armand@poli.ufrj.br>
2021-04-05 23:21:20 -0300

    Create suport_functions.py

---
+++ suport_functions.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_latent_space(func, n=20, figsize=15):
+    # Initial func test to get image shape:
+    z = np.array([[0, 0]])
+    x = func(z)
+    digit_size = len(x[0])
+    print(digit_size)
+
+    # display a n*n 2D manifold of digits
+    scale = 1.0
+    figure = np.zeros((digit_size * n, digit_size * n))
+    # linearly spaced coordinates corresponding to the 2D plot
+    # of digit classes in the latent space
+    grid_x = np.linspace(-scale, scale, n)
+    grid_y = np.linspace(-scale, scale, n)[::-1]
+
+    for i, yi in enumerate(grid_y):
+        for j, xi in enumerate(grid_x):
+            z_sample = np.array([[xi, yi]])
+            x_decoded = func(z_sample)
+            try:
+              digit = x_decoded[0].reshape(digit_size, digit_size)
+            except:
+              digit = x_decoded[0][:,:,0]
+            figure[
+                i * digit_size : (i + 1) * digit_size,
+                j * digit_size : (j + 1) * digit_size,
+            ] = digit
+
+    plt.figure(figsize=(figsize, figsize))
+    start_range = digit_size // 2
+    end_range = n * digit_size + start_range
+    pixel_range = np.arange(start_range, end_range, digit_size)
+    sample_range_x = np.round(grid_x, 1)
+    sample_range_y = np.round(grid_y, 1)
+    plt.xticks(pixel_range, sample_range_x)
+    plt.yticks(pixel_range, sample_range_y)
+    plt.xlabel("z[0]")
+    plt.ylabel("z[1]")
+    plt.imshow(figure, cmap="Greys_r")
+    plt.show()
